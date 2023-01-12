4f89a5a5c3b8955420449d61593c9c3485b2f986
Noah do Rego <ndore051@uottawa.ca>
2023-01-11 22:58:27 -0500

    Created folder and files to simplify App.js and improve structure [incomplete]

---
+++ app/styling/colors.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default colors = StyleSheet.create({
+    black: { backgroundColor: "#000" },
+    darkGrey: { backgroundColor: "#212121" },
+    grey: { backgroundColor: '#4f4f4f' },
+    lightGrey: { backgroundColor: '#767577' },
+    green: { backgroundColor: "#47b356" },
+    blue: { backgroundColor: "#325ea8" },
+    red: { backgroundColor: "#e85b51" },
+    white: { backgroundColor: "#fff" },
+    whiteText: { color: "#fff" },
+});
---
+++ app/styling/margins.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+export default margins = StyleSheet.create({
+    m10: { margin: 10 },
+    m12: { margin: 12 },
+    m20: { margin: 20 },
+});
